f3c7c71ac45c8e7bea4e5ae21168673b13c5613b
danielpraisethelord <danielyosef777@gmail.com>
2025-10-05 14:45:41 -0600

    chore: resolve TODO

--- src/main/java/com/foodtruck/backend/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/com/foodtruck/backend/infrastructure/security/CustomUserDetailsService.java
@@ -13,8 +13,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO: Buscar usuario con username en lowercase
-        return repo.findByUsername(username)
+        return repo.findByUsername(username.trim().toLowerCase())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
     }
 }
